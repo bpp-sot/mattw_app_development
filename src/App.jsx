0d4bdfd1cf63b507aca3c83af858394ac5e1bc23
Matt-Wallbank <m.wallbank@my.bpp.com>
2025-10-26 15:04:40 +0000

    Initial App Development, adding HTML elements and associated CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,26 +6,31 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div class="header">
+        <img id="logoImage" src="Servaada Logo.png" alt="Servaada Logo" />
+        <h1 id ="pageTitle"> File Comparison Tool </h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR - Has this worked?
-        </p>
+      <br></br>
+      <br></br>
+      <div class="topnav">
+        <a href="#home" class="active">Home</a>
+        <a href="#news">News</a>
+        <a href="#contact">Contact</a>
+        <a href="#about">About</a>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div id ="pageinformation">Upload your chosen files below, and click "Compare Files" Button</div>
+      <div class ="fileuploads">   
+         <h2 class ="upload-titles">Upload Source File</h2>
+          <input id="srcFileUpload" class ="fileUploads" type="file" name="srcFileUpload" />
+          <input id="srcFileSubmit" class = "buttons" type="submit" value="Upload"  onclick="alert('File Submitted')" />
+          <br></br>
+          <h2 class ="upload-titles">Upload Target File</h2>
+          <input id="tgtFileUpload" class ="fileUploads" type="file"  name="tgtFileUpload" />
+          <input id="tgtFileSubmit" class = "buttons" type="submit" value="Upload"  onclick="alert('File Submitted')"/>
+          <br></br>
+          <br></br>
+          <button id="compareFilesButton" class="buttons" onclick="alert('Compare Files clicked')">Compare Files</button>
+      </div>
     </>
   )
 }
