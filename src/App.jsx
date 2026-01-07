6ca7a392d2d8f8b9b665bfdc6d00ed19c6c799ea
Matt-Wallbank <m.wallbank@my.bpp.com>
2026-01-07 19:10:16 +0000

    Addition of Footer bar and associated CSS

--- src/App.jsx
+++ src/App.jsx
@@ -30,18 +30,41 @@ function App() {
       </Navbar>
       <div id ="pageinformation">Upload your chosen files below, and click "Compare Files" Button</div>
       <div className ="fileuploads">   
-         <h2 className ="upload-titles">Upload Source File</h2>
+         <h2 className ="upload-titles">Upload Baseline File</h2>
           <input id="srcFileUpload" className = "fileUploads" type="file" name="srcFileUpload" />
           <input id="srcFileSubmit" className = "buttons" type="submit" value="Upload"/>
           <br></br>
           <br></br>
-          <h2 className ="upload-titles">Upload Target File</h2>
+          <h2 className ="upload-titles">Upload Comparison File</h2>
           <input id="tgtFileUpload" className ="fileUploads" type="file"  name="tgtFileUpload" />
           <input id="tgtFileSubmit" className = "buttons" type="submit" value="Upload"/>
           <br></br>
           <br></br>
           <button id="compareFilesButton" className="buttons">Compare Files</button>
       </div>
+      <footer id="footer" class="bg-light text-black pt-4 pb-2">
+        <div class="container">
+            <div class="row">
+              <div class="col-md-4 text-black">
+                <h5>About Us</h5>
+                <p class="text-black">
+                  We help our clients provide excellent customer service to their retail and corporate customers – while optimising efficiency, reducing operational risk, and operating consistently within the FCA’s strict regulatory framework.
+                </p>
+              </div>
+              <div class="col-md-4">
+                <h5>Quick Links</h5>
+                <ul class="list-unstyled text-black">
+                  <li><a href="https://www.wipro.com/servaada/" class="text-black">Home</a></li>
+                  <li><a href="https://www.wipro.com/applications/" class="text-black">Applications</a></li>
+                  <li><a href="https://www.wipro.com/contact-wipro/" class="text-black">Contact</a></li>
+                </ul>
+              </div>
+            <div class="text-center">
+              <p class="mb-0 text-black">&copy; 2026 Servaada. All rights reserved.</p>
+            </div>
+          </div>
+        </div>
+      </footer>
     </>
   )
 }
